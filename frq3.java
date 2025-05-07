3630c63e9a48b7e6860efeb345fa9b3faa967a9c
Taurox Chao <tauroxchao2@gmail.com>
2025-05-06 20:43:53 -0400

    meow

--- frq3.java
+++ frq3.java
@@ -1,6 +1,14 @@
-
+import java.util.ArrayList;
 public class frq3{
     public static class WordChecker{
         private ArrayList<String> wordList;
+        public WordChecker(ArrayList<String> e){
+            wordList = e;
+        }
+        public boolean isWordChain(){
+            for (int i = 1; i < wordList.size(); i++){
+                if ()
+            }
+        }
     }
 }
